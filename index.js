3acdd35dce10b889de3f55f7e7cddcd45202d944
jatin34090 <jatin34090@gmail.com>
2024-05-23 20:06:25 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const app = express();
+require('dotenv').config()
+
+app.use(express.static('dist'))
+
+const PORT = 5000 || process.env.PORT
+
+mongoose.connect(process.env.MONGOURI).then(()=>{
+    
+    console.log("Connected to mongoDB")
+})
+.catch((err) => {
+    console.log("Error connecting to mongoDB", err)
+})
+
+
+app.use(express.json())
+app.use(require('./routes/auth'))
+app.use(require('./routes/post'))
+app.use(require('./routes/user'))
+
+
+
+app.listen(PORT, () => {
+    console.log("server is running on port 5000")
+})
